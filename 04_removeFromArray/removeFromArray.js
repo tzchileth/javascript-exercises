b40dad79252bf94e371d38f21cbabb7841a3cf12
Achile Egbunu <achilesolomon@gmail.com>
2023-07-04 15:15:45 -0500

    Remove from array pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, param) {
+    // get the number of parameters passed to the function
+    let argumentLength = arguments.length;
 
+    if (argumentLength >= 2) {
+        let currentIndex = 1;
+        while (currentIndex < argumentLength) {
+            let result = array.indexOf(arguments[currentIndex]);
+            if (result != -1) {
+                array = array.slice(0, result).concat(array.slice(result + 1));
+                currentIndex++;
+            } else {
+                currentIndex++;
+            }
+        }
+        return array;
+    }
 };
 
 // Do not edit below this line
