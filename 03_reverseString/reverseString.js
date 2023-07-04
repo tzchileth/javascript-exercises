89b3e1b8b81cb8ac110fbd4f270ca2a206097146
Achile Egbunu <achilesolomon@gmail.com>
2023-07-04 14:09:44 -0500

    Reverse string pass

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let reversed = "";
+    const STRING_LENGTH = string.length - 1;
 
+    for (let i = STRING_LENGTH; i >= 0; i--) {
+        reversed += string[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
