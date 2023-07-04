ec005a50555b71f7917325c3fd84b2549348f3f3
Achile Egbunu <achilesolomon@gmail.com>
2023-07-04 15:40:18 -0500

    Sum all pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (startInt, endInt) {
+    let sum = 0;
+    if (startInt < 0 || endInt < 0 || typeof (startInt) !== 'number' || typeof (endInt) !== 'number') {
+        return 'ERROR';
+    } else {
+        if (startInt < endInt) {
+            for (let i = startInt; i <= endInt; i++) {
+                sum += i;
+            }
+        } else if (startInt > endInt) {
+            for (let i = startInt; i >= endInt; i--) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
