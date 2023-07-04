9495a6daf24d2d118eeaefe41b47f6ea75edb5fa
Achile Egbunu <achilesolomon@gmail.com>
2023-07-04 13:48:26 -0500

    Repeat string passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (userString, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    else if (num === 0 && typeof (userString === "string")) {
+        return '';
+    } else if (num > 0 && typeof (userString === "string")) {
+        let result = "";
+        for (let i = 1; i <= num; i++) {
+            result += userString;
+        }
+        return result;
+    }
 };
 
 // Do not edit below this line
